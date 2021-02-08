a3031ec57db780eba99149f3df0b486d07387f7c
Mark <SwiftTactics@gmail.com>
2021-02-08 01:56:51 -0500

    added external library Series with JAR file

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i =0; i <= 10; i++){
+            System.out.println("nSum of " + i + " = "+ Series.nSum(i));
+            System.out.println("factorial of " + i + " = "+ Series.factorial(i));
+            System.out.println("fibonacci of " + i + " = "+ Series.fibonacci(i));
+
+
+        }
+
+    }
+}
